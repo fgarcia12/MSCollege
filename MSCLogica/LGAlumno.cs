73aad9be86e89905277c0b07f34dd6812a8f050f
Francisco Garcia <fagg006@gmail.com>
2015-07-09 19:23:12 -0600

    Mensjae

--- MSCLogica/LGAlumno.cs
+++ MSCLogica/LGAlumno.cs
@@ -4,20 +4,21 @@ using System.Linq;
 using System.Text;
 using MSCComun;
 using System.Windows.Forms;
+using MSCLogica;
 
 namespace MSCLogica
 {
 
     public class LGAlumno
     {
+        
      
         public  void ValidaAlumno(MSCComun.CMAlumno Alumno)
         {
            
             if (String.IsNullOrEmpty(Alumno.AL_CARNET1))
             {
-            
-            
+                CM_Utilidades.MensajeBox("Carnet del alumno esta en blanco", true);
             }
             
         }
